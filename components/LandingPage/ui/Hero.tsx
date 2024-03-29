07616283fe642bab7cb26b2c91e2f751934dab38
Arman Kumar Jena <arman.k.jena07@gmail.com>
2024-03-29 09:35:47 +0530

    added hero section

---
+++ components/LandingPage/ui/Hero.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <section className="bg-[#666666] lg:h-[calc(100vh-64px)] max-w-full">
+      <article className="">sss</article>
+    </section>
+  );
+};
+
+export default Hero;
